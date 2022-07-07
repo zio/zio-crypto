d99ab13cbc3811ddf8e72177c4a930cdc521266f
Mark Rudolph <mark@k8ty.app>
2022-07-06 21:49:54 -0400

    Update some dependencies for new scala matrix
    SilencerVersion => 1.7.8 (matching core zio lib)
    sbt-scalafix => 0.10.0 (mathcing core zio lib)
    kind-projector => 0.13.2 (bumped to latest)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -27,7 +27,7 @@ object BuildHelper {
   val Scala213: String   = versions("2.13")
   val ScalaDotty: String = versions("3.1")
 
-  val SilencerVersion = "1.7.5"
+  val SilencerVersion = "1.7.8"
 
   private val stdOptions = Seq(
     "-deprecation",
@@ -239,7 +239,7 @@ object BuildHelper {
         Seq(
           "com.github.ghik" % "silencer-lib"            % SilencerVersion % Provided cross CrossVersion.full,
           compilerPlugin("com.github.ghik" % "silencer-plugin" % SilencerVersion cross CrossVersion.full),
-          compilerPlugin("org.typelevel"  %% "kind-projector"  % "0.13.0" cross CrossVersion.full)
+          compilerPlugin("org.typelevel"  %% "kind-projector"  % "0.13.2" cross CrossVersion.full)
         )
     },
     semanticdbEnabled := scalaVersion.value != ScalaDotty, // enable SemanticDB
