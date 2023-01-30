29edabf1439be5ad7ae1909f448e0940afe7db01
Milad Khajavi <khajavi@gmail.com>
2023-01-30 12:52:42 +0330

    remove macro expansion settings.

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -201,22 +201,6 @@ object BuildHelper {
     unusedCompileDependenciesFilter -= moduleFilter("org.scala-js", "scalajs-library")
   )
 
-  def macroExpansionSettings = Seq(
-    scalacOptions ++= {
-      CrossVersion.partialVersion(scalaVersion.value) match {
-        case Some((2, 13)) => Seq("-Ymacro-annotations")
-        case _             => Seq.empty
-      }
-    },
-    libraryDependencies ++= {
-      CrossVersion.partialVersion(scalaVersion.value) match {
-        case Some((2, x)) if x <= 12 =>
-          Seq(compilerPlugin(("org.scalamacros" % "paradise" % "2.1.1").cross(CrossVersion.full)))
-        case _                       => Seq.empty
-      }
-    }
-  )
-
   val scalaReflectTestSettings: List[Setting[_]] = List(
     libraryDependencies ++= {
       if (scalaVersion.value == Scala3)
