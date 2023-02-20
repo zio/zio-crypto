31e42574eb4f1be985e5bb77386d4d84516a8883
Milad Khajavi <khajavi@gmail.com>
2023-02-20 18:23:54 +0330

    fix versions.

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,4 +3,13 @@ object Versions {
   val Scala212 = "2.12.15"
   val Scala213 = "2.13.8"
   val Scala3   = "3.2.1"
+
+  val SilencerVersion = "1.7.12"
+
+  val GoogleCloudKMSVersion = "2.12.0"
+  val TinkVersion           = "1.7.0"
+  val ZIOVersion            = "2.0.0"
+  val AWSKMSVersion         = "1.12.395"
+  val IzumiReflectVersion   = "2.1.0"
+  val ZIOStacktracerVersion = "2.0.0"
 }
