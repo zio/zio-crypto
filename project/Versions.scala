f036a6170f7934de31905fe2862ba5399ef959da
Scala Steward <scala_steward@virtuslab.com>
2023-06-03 10:42:03 +0000

    Update zio, zio-stacktracer, zio-test, ... to 2.0.14

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,5 +4,5 @@ object Versions {
   val tinkVersion           = "1.7.0"
   val awsKMSVersion         = "1.12.411"
   val izumiReflectVersion   = "2.1.0"
-  val zioStacktracerVersion = "2.0.0"
+  val zioStacktracerVersion = "2.0.14"
 }
