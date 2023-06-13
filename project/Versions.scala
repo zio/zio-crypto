702e4642ab135c35348a61755bda69d6e28e0186
Scala Steward <scala_steward@virtuslab.com>
2023-06-13 02:42:26 +0000

    Update silencer-lib, silencer-lib_2.13.10, ... to 1.7.13

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  val SilencerVersion       = "1.7.12"
+  val SilencerVersion       = "1.7.13"
   val googleCloudKMSVersion = "2.14.0"
   val tinkVersion           = "1.7.0"
   val awsKMSVersion         = "1.12.411"
