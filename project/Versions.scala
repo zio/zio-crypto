173261edd39c166a4e43d9716d9b3c19d0e0ce2e
Scala Steward <scala_steward@virtuslab.com>
2024-04-09 02:52:50 +0000

    Update tink to 1.13.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val SilencerVersion       = "1.7.12"
   val googleCloudKMSVersion = "2.14.0"
-  val tinkVersion           = "1.7.0"
+  val tinkVersion           = "1.13.0"
   val awsKMSVersion         = "1.12.411"
   val izumiReflectVersion   = "2.1.0"
   val zioStacktracerVersion = "2.0.0"
